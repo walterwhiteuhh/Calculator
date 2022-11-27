371b8aa3de0b3c7f53609af9c33d3cf361988335
Timo <timo.fischer@protonmail.com>
2022-11-27 03:22:51 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import modules
+
+from tkinter import *
+import math
+import tkinter.messagebox
+
+root = Tk()
+root.title("Scientific Calculator")
+root.configure(background = 'white')
+root.resizable(width=False, height=False)
+root.geometry("480x568+450+90")
+
+root.mainloop()
