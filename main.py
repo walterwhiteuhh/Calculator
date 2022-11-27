bd068e913af890bb9c6b9d94fd95142f86dccfd4
Timo <timo.fischer@protonmail.com>
2022-11-27 03:30:47 +0100

    Minor changes on main.py

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from tkinter import *
 import math
 import tkinter.messagebox
 
+
 root = Tk()
 root.title("Scientific Calculator")
 root.configure(background = 'white')
 root.resizable(width=False, height=False)
 root.geometry("480x568+450+90")
+
+calc = Frame(root)
+
+calc.grid()
+
 
 root.mainloop()
